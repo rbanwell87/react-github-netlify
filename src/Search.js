c2539d303921e819cffc73065e1b3824947fe9c1
rbanwell87 <robin.banwell@gmail.com>
2021-04-06 18:51:31 -0400

    Updated page name

--- src/Search.js
+++ src/Search.js
@@ -5,7 +5,7 @@ export default function Search() {
   return (
     <form>
       <input type="search" placeholder="Search a city..."></input>
-      <input type="submit" value="search" className="btn" />
+      <input type="submit" value="search" className="btn btn-primary w-100" />
     </form>
   );
 }
