64dbf1ba6849d005c4cfcf3290b6eb80c9be3e3f
rbanwell87 <robin.banwell@gmail.com>
2021-04-06 17:54:56 -0400

    Added Search Form

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Weather App</h1>
-        <p>This is gonna be a cool app.</p>
-        <small>
-          <a
-            className="Github-link"
-            href="https://github.com/rbanwell87/react-github-netlify/settings"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open-source code
-          </a>{" "}
-          by Robin Banwell
-        </small>
-      </header>
+      <h1>React Weather App</h1>
+      <form>
+        <input type="search" placeholder="Search a city..."></input>
+        <input type="submit" value="search" class="btn btn-primary w-100" />
+      </form>
+      <br />
+      <br />
+      <small>
+        <a
+          className="Github-link"
+          href="https://github.com/rbanwell87/react-github-netlify/settings"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Robin Banwell
+      </small>
     </div>
   );
 }
