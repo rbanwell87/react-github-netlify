f2b9d165a13590f5b2fd0b9506f6019c64c16184
rbanwell87 <robin.banwell@gmail.com>
2021-04-06 14:04:31 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Weather App</h1>
+        <p>This is gonna be a cool app.</p>
+        <small>
+          <a
+            className="Github-link"
+            href="https://github.com/rbanwell87/Final-Project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Robin Banwell
+        </small>
+      </header>
+    </div>
+  );
+}
+
+export default App;
