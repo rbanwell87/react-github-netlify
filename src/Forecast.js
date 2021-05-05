410d78c6064c95b737c5b2858bfdd41637943d98
rbanwell87 <robin.banwell@gmail.com>
2021-05-05 12:00:29 -0400

    finishing touches

--- src/Forecast.js
+++ src/Forecast.js
@@ -27,6 +27,8 @@ export default function Forecast(props) {
                   <ForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
