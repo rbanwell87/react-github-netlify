d2b2eeb0ac71661625516e60d800580258fcb70e
rbanwell87 <robin.banwell@gmail.com>
2021-05-05 12:19:57 -0400

    fix error

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
       icon={codeMapping[props.code]}
       color="white"
       size={props.size}
-      animate="true"
+      animate={true}
     />
   );
 }
